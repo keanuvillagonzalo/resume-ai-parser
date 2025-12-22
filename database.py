2761b58c0625ce5ca6b327200a6a7bb9360d553f
Keanu Villagonzalo <kvillagonzalo27@g.ucla.edu>
2025-12-21 23:30:20 -0500

    Removed unused functions in database.py

--- database.py
+++ database.py
@@ -9,41 +9,6 @@ API_KEY=os.getenv("API_KEY")
 DATABASE_URL=os.getenv("DATABASE_URL")
 
 
-def init_db():
-
-    conn = sqlite3.connect(DATABASE_URL)
-
-    c = conn.cursor()
-
-    # Create table to store the raw JSON experiences per resume
-
-    c.execute('''CREATE TABLE IF NOT EXISTS parsed_resumes 
-
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-
-                  filename TEXT, 
-
-                  experience_json TEXT)''')
-
-    conn.commit()
-
-    conn.close()
-
-
-def save_experiences(filename, data_list):
-
-    conn = sqlite3.connect(DATABASE_URL)
-
-    c = conn.cursor()
-
-    c.execute("INSERT INTO parsed_resumes (filename, experience_json) VALUES (?, ?)", 
-
-              (filename, json.dumps(data_list)))
-
-    conn.commit()
-
-    conn.close()
-
 
 def test_db_connection():
 
